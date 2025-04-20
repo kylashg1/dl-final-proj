6d3dcf82868f59594ebaf6170da0fe1373cdde47
adeethyia-shankar <adeethyia_shankar@brown.edu>
2025-04-20 17:58:23 -0400

    Trying to get GPU working

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def main():
-    import tensorflow as tf
     print(f"Num GPUs Available: {len(tf.config.list_physical_devices('GPU'))}")
 
 if __name__ ==  "__main__":
