6d109df7ae64d6b98b1a73da11af00d350b9e012
adeethyia-shankar <adeethyia_shankar@brown.edu>
2025-04-21 16:33:30 -0400

    Added command line args

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ def parse_args():
     parser = ArgumentParser(description='“Transforming” Protein sequences into images humans can understand!')
     parser.add_argument('--data', required=True, type=str, help='File path to data csv')
     parser.add_argument('--epochs', required=True, type=int, help='Number of epochs used in training')
+    return parser.parse_args()
 
 def main(args):
     print(f'Data CSV file path: {args.data}')
     print(f'Epochs: {args.epochs}')
-    print(f'Num GPUs Available: {len(tf.config.list_physical_devices('GPU'))}')
+    print(f'Num GPUs Available: {len(tf.config.list_physical_devices("GPU"))}')
 
 if __name__ ==  "__main__":
     main(parse_args())
