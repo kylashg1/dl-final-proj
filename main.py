d1d58e8a5f0c777e5b45e02d63e56951b667b35e
andy <huy_le@brown.edu>
2025-04-27 17:30:56 -0400

    adding to main

--- main.py
+++ main.py
@@ -14,20 +14,24 @@ def main():
     # dataloader.populate_inputs("unprocessed_data", "processed_data")
     # dataloader.populate_csv("data.csv")
     
+    
     processed_dataset = data.OpenCellLoaderTF("data.csv")
-    dataset = processed_dataset.get_dataset()
-    dataset = dataset.batch(3)
-    for batch in dataset:
-        nucleus = batch['nucleus']  # Shape: (batch_size, 256, 256, 1)
-        target = batch['target']    # Shape: (batch_size, 256, 256, 1)
-        threshold = batch['threshold']  # Shape: (batch_size, 256, 256, 1)
-        sequence = batch['sequence']  # Shape: (batch_size, 1, 1001, 256)
+    encoded_sequence = processed_dataset._encode_sequence("ALMQQN")
+    print(f"encoded sequence {encoded_sequence}, shape {encoded_sequence.shape}")
+
+    # dataset = processed_dataset.get_dataset()
+    # dataset = dataset.batch(3)
+    # for batch in dataset:
+    #     nucleus = batch['nucleus']  # Shape: (batch_size, 256, 256, 1)
+    #     target = batch['target']    # Shape: (batch_size, 256, 256, 1)
+    #     threshold = batch['threshold']  # Shape: (batch_size, 256, 256, 1)
+    #     sequence = batch['sequence']  # Shape: (batch_size, 1, 1001, 256)
         
-        # Now you can use the batch data for training or evaluation
-        print(nucleus.shape)  # Should print (batch_size, 256, 256, 1)
-        print(target.shape)  # Should print (batch_size, 256, 256, 1)
-        print(threshold.shape)  # Should print (batch_size, 256, 256, 1)
-        print(sequence.shape)  # Should print (batch_size, 1, 1001, 256)
+    #     # Now you can use the batch data for training or evaluation
+    #     print(nucleus.shape)  # Should print (batch_size, 256, 256, 1)
+    #     print(target.shape)  # Should print (batch_size, 256, 256, 1)
+    #     print(threshold.shape)  # Should print (batch_size, 256, 256, 1)
+    #     print(sequence.shape)  # Should print (batch_size, 1, 1001, 256)
 
     
 if __name__ ==  "__main__":
