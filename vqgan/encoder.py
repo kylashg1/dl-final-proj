6622091d477100c584cdde195adb84eeb78a23fe
Kylash Ganesh <kylash_ganesh@brown.edugit config --global user.name kylashg1>
2025-04-28 00:28:35 -0400

    image showing up, vqgan is now traning

--- vqgan/encoder.py
+++ vqgan/encoder.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 # CNN encoder for proteign images
 def encoder(latent_dim=128):
     encoder = tf.keras.Sequential([
-        tf.keras.layers.Input(shape=(256, 256, 1)),  # Assuming 64x64 thresholded input and is grayscale
+        tf.keras.layers.Input(shape=(256, 256, 1)),  # Assuming 256x256 thresholded input and is grayscale
         tf.keras.layers.Conv2D(64, 4, strides=2, padding='same', activation='relu'),
         tf.keras.layers.Conv2D(128, 4, strides=2, padding='same', activation='relu'),
         tf.keras.layers.Conv2D(256, 4, strides=2, padding='same', activation='relu'),
