8d15392ac4850fed6f806e89442c01100cf12624
adeethyia-shankar <adeethyia_shankar@brown.edu>
2025-04-27 23:28:47 -0400

    Didn't change any code

--- celle_assignment.py
+++ celle_assignment.py
@@ -3,6 +3,17 @@ import dataloader as data
 from vqgan import VQGAN, VectorQuantizer
 import tensorflow as tf
 
+# Creating + training the model - training data should be in (batch_size, 64, 64, 1) normalized to [-1, 1]
+def train_vqgan(train_dataset):
+    vqgan = VQGAN(latent_dim=128)
+    # Compiling the model with optimizer and loss
+    vqgan.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+        loss=tf.keras.losses.MeanSquaredError(), # Reconstruction loss
+    )
+    vqgan.fit(train_dataset, epochs=100) # Training the model
+    vqgan.save('vqgan_model') # Saving the trained model
+
 def main():
     # Preprocess
 
@@ -11,12 +22,10 @@ def main():
     # processed_data.download_cell_imgages(3) # download function for OpenCell AWS server - only argument is how many TOTAL images you want to download, None means download ALL images
     # processed_data.populate_csv("data.csv") # puts processed .png images into the csv and searched up the corresponding amino acid sequence and also puts into data.csv
 
-
     # creates the vectorized dataset to pass into VQGAN
     dataset_tensor = data.OpenCellLoaderTF("data.csv", crop_size=64)
     dataset_tensor = dataset_tensor.get_dataset()
     dataset_tensor = dataset_tensor.batch(3)
-
 
     input = None
     threshold = None
@@ -34,7 +43,6 @@ def main():
     print(threshold.shape)  # (batch_size, 256, 256, 1)
     print(sequence.shape)  # (batch_size, 1, 1001, 256)
 
-
     # VQGAN
     # Loading in trained vqgan
     vqgan = tf.keras.models.load_model('vqgan_model', custom_objects={'VectorQuantizer': VectorQuantizer})
@@ -48,24 +56,6 @@ def main():
     # threshold_vqgan = VQGAN() # VQGAN(encoder(), decoder(), VectorQuantizer(codebook_size=512, code_dim=128, commitment_cost=.25))
     threshold_data = vqgan(threshold)
     print(f"output {threshold_data}")
-
-
-# Creating + traning the model - training data should be in (batch_size, 64, 64, 1) normalized to [-1, 1]
-def train_vqgan(train_dataset):
-    vqgan = VQGAN(latent_dim=128)
-
-    # Compiling the model with optimizer and loss
-    vqgan.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
-        loss=tf.keras.losses.MeanSquaredError(),  # Reconstruction loss (MSE between input and output images)
-    )
-
-    # Training the model
-    vqgan.fit(train_dataset, epochs=100)
-
-    # Saving the trained model
-    vqgan.save('vqgan_model')
-
 
 if __name__ ==  "__main__":
     main()
