d9de6fa44b05628a9cd16c5b0df8c65a81c41a00
adeethyia-shankar <adeethyia_shankar@brown.edu>
2025-04-27 23:59:43 -0400

    Changed decoder

--- vqgan.py
+++ vqgan.py
@@ -52,7 +52,9 @@ class VQGAN(tf.keras.Model):
             tf.keras.layers.Input(shape=(16, 16, latent_dim)),  # Depends on encoder compression
             tf.keras.layers.Conv2DTranspose(128, 4, strides=2, padding='same', activation='relu'),
             tf.keras.layers.Conv2DTranspose(64, 4, strides=2, padding='same', activation='relu'),
-            tf.keras.layers.Conv2D(1, 3, strides=1, padding='same', activation=None)  # 1 output channel as logits
+            tf.keras.layers.Conv2DTranspose(32, 4, strides=2, padding='same', activation='relu'),
+            tf.keras.layers.Conv2DTranspose(16, 4, strides=2, padding='same', activation='relu'),
+            tf.keras.layers.Conv2D(1, 3, strides=1, padding='same', activation=None) # 1 output channel as logits
         ], name="Decoder")
         self.vquantizer = VectorQuantizer(codebook_size=512, code_dim=128, commitment_cost=.25)
 
